8dcbf5614eb0d50774731ea853f3ba3f6385aec2
isadc <isadc@DESKTOP-S0PQ6K5>
2020-03-16 13:39:38 +0100

    Delete residence

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -58,10 +58,10 @@ public class Pet extends NamedEntity {
 	@JoinColumn(name = "owner_id")
 	private Owner owner;
 
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.EAGER)
+	@OneToMany(cascade = CascadeType.REMOVE, mappedBy = "pet", fetch = FetchType.EAGER)
 	private Set<Visit> visits;
 	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.EAGER)
+	@OneToMany(cascade = CascadeType.REMOVE, mappedBy = "pet", fetch = FetchType.EAGER)
 	private Set<Residence> residences;
 
 	public void setBirthDate(LocalDate birthDate) {
